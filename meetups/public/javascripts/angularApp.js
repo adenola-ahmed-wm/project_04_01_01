4f45499f5b7321d0b2de4b44ede4d044e49403c8
DM <session2@START-Coding-MBP-16.local>
2017-02-22 18:13:54 -0700

    Speakers & speakers summary works, just need restructuring

--- meetups/public/javascripts/angularApp.js
+++ meetups/public/javascripts/angularApp.js
@@ -1,13 +1,19 @@
 var myApp = angular.module('myApp', [
     //Dependencies of what the application needs to run
-    'ngRoute'
+    'ngRoute',
+    'meetupsControllers'
 ]);
 
 myApp.config([
     '$routeProvider',
     function($routeProvider){
         $routeProvider.when('/home', {
-            templateUrl: 'partials/home.ejs'
+            templateUrl: 'partials/home.ejs',
+            controller: 'HomeController'
+        })
+        .when('/speakers', {
+            templateUrl: 'partials/speakers.ejs',
+            controller: 'HomeController'
         })
         .otherwise({
             redirectTo: '/home'
