5c3d07d767c4811d770c68fcbd6bead60275a21b
Angel Iniquez <angel.iniguez@west-mec.org>
2017-02-23 18:15:38 -0700

    updated the angularapp and controller for details

--- meetups/public/javascripts/angularApp.js
+++ meetups/public/javascripts/angularApp.js
@@ -1,19 +1,23 @@
 var myApp = angular.module('myApp', [
     //Dependencies of what the application needs to run
     'ngRoute',
-    'meetupsControllers'
+	'meetingController'
 ]);
 
 myApp.config([
     '$routeProvider',
     function($routeProvider){
         $routeProvider.when('/home', {
-            templateUrl: 'partials/home.ejs',
+           templateUrl: 'partials/home.ejs',
             controller: 'HomeController'
         })
         .when('/speakers', {
-            templateUrl: 'partials/speakers.ejs',
+            templateUrl: 'partials/speakerslist.ejs',
             controller: 'HomeController'
+        })
+        .when('/details/:speakersid', {
+            templateUrl: 'partials/speakerslist.ejs',
+            controller: 'detailsController'
         })
         .otherwise({
             redirectTo: '/home'
--- meetups/public/javascripts/controller.js
+++ meetups/public/javascripts/controller.js
@@ -10,4 +10,24 @@ meetupsControllers.controller('HomeController', ['$scope', '$http', function ($s
     function(result){
       console.log('HTTP request failed with result: ' + result);
     });
+    $scope.speakerslist = true;
+}]);
+
+meetupsControllers.controller('detailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+  $http.get('data.json').then(function(response){
+    var myArtists = [];
+    var myArtwork = [];
+    response.datails.speakers.forEach(function(item) {
+      // looping 
+      if (item.shortname == $routeParams.speakersid) {
+        myArtists.push(item);
+        myArtwork = myArtwork.concat(item.artwork);
+      }
+    });
+    $scope.artists = myArtists;
+    $scope.speakersDetail = true;
+  },
+    function(result) {
+      console.log('http request faild iwth result :' + result);
+    });
 }]);
